b7b2b7da1ffaf99f0b22efedd6093ef89d1b424e
affigueir <affigueir@gmail.com>
2022-06-23 10:57:36 +0100

    IncDec added

---
+++ IncDec/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace IncDec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1, b = 2, c, d;
+
+            c = a++ + b++;
+            d = ++a + ++b;
+
+            Console.WriteLine($"a = {a} | b = {b} \nSuffix sum = {c} | Prefix sum = {d}");
+
+            System.Console.WriteLine("_________________________________________________");
+
+            int e = 4, f = 5, g, h;
+
+            g = e-- - f--;
+            h = --e - --f;
+
+            Console.WriteLine($"e = {e} | f = {f} \nSuffix subtr = {g} | Prefix subtr = {h}");
+        }
+    }
+}
