e064c1edbe16148ef01c4b88e946911664cb9e6e
affigueir <affigueir@gmail.com>
2022-06-26 15:56:34 +0100

    testing working

--- testing/Program.cs
+++ testing/Program.cs
@@ -7,19 +7,25 @@ namespace testing
         static void Main(string[] args)
         {
             int a = 0;
+            bool loop = false;
 
-            Console.Write("Insert a number: ");
-            string input = Console.ReadLine();
-            bool result = int.TryParse(input, out a);
+            while (loop == false)
+            {
+                Console.Write("Insert a number: ");
+                string input = Console.ReadLine();
+                bool result = int.TryParse(input, out a);
 
             if (result == true)
             {
                 int i = int.Parse(input);
                 System.Console.WriteLine($"Your number is {i}");
+                break;
             }
             else
             {
-                System.Console.WriteLine("That is not a number!");
+                System.Console.WriteLine($"{input} is not a number!");
+                System.Console.WriteLine("_________________________");
+            }
             }
 
             
