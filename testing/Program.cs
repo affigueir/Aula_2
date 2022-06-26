c48beb683ebb729fb0643be187a57b1baddba41b
affigueir <affigueir@gmail.com>
2022-06-26 15:48:47 +0100

    replaced tests

---
+++ testing/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+
+            Console.Write("Insert a number: ");
+            string input = Console.ReadLine();
+            bool result = int.TryParse(input, out a);
+
+            if (result == true)
+            {
+                int i = int.Parse(input);
+                System.Console.WriteLine($"Your number is {i}");
+            }
+            else
+            {
+                System.Console.WriteLine("That is not a number!");
+            }
+
+            
+        }
+    }
+}
