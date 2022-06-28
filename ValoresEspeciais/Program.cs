fbb43992d0656b2bebcb98cde4d496065319462b
affigueir <affigueir@gmail.com>
2022-06-28 14:22:00 +0100

    Add overflow egs

--- ValoresEspeciais/Program.cs
+++ ValoresEspeciais/Program.cs
@@ -14,10 +14,22 @@ namespace ValoresEspeciais
             double negativeInfinity = double.NegativeInfinity;
             double not = double.NaN;
 
+            int max = int.MaxValue;
+            int overMax = max + 1;
+
+            double megaMax = double.MaxValue;
+            double ultraMax = 2 * megaMax;
+
+            float normalMax = float.MaxValue;
+            float sameMax = normalMax + 1;
 
             System.Console.WriteLine($"{a}\n{b}\n{c}\n{d}\n{e}\n{f}\n"); 
             System.Console.WriteLine("-----------------------------");
             System.Console.WriteLine($"{positiveInfinity}\n{negativeInfinity}\n{not}");
+            System.Console.WriteLine("-----------------------------");
+            System.Console.WriteLine($"{max}\n{overMax}");
+            System.Console.WriteLine("-----------------------------");
+            System.Console.WriteLine($"{megaMax}\n{ultraMax}\n\n{normalMax}\n{sameMax}\n");
         }
     }
 }
