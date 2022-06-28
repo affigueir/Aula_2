b30fe85f7ac154b55cea376d81b65c94e6aa27e5
affigueir <affigueir@gmail.com>
2022-06-28 14:05:58 +0100

    Add ValoresEspeciais

---
+++ ValoresEspeciais/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ValoresEspeciais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.MaxValue, b = int.MinValue;
+            uint c = uint.MaxValue, d = uint.MinValue;
+            double e = double.MaxValue, f = double.MinValue;
+
+            double positiveInfinity = double.PositiveInfinity;
+            double negativeInfinity = double.NegativeInfinity;
+            double not = double.NaN;
+
+
+            System.Console.WriteLine($"{a}\n{b}\n{c}\n{d}\n{e}\n{f}\n"); 
+            System.Console.WriteLine("-----------------------------");
+            System.Console.WriteLine($"{positiveInfinity}\n{negativeInfinity}\n{not}");
+        }
+    }
+}
